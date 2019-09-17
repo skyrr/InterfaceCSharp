e22f52942a13b127d8d370738f627149656c1366
Volodymyr Neborachko <Volodymyr_Neborachko@epam.com>
2019-09-17 10:51:15 +0300

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person u = new User();
+            Person t = new Teacher();
+
+            u.Say();
+            t.Say();
+
+            Console.Read();
+        }
+    }
+    public interface Person
+    {
+        void Say();
+    }
+    class User : Person
+    {
+        public void Say()
+        {
+            Console.WriteLine("I am a User");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public void Say()
+        {
+            Console.WriteLine("I am a Teacher");
+        }
+    }
+}
